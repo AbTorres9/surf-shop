0e5bc41aaa4e5542913cca870ed81537a8fb4b39
AbTorres9 <you@example.com>
2020-05-12 17:09:48 +0530

    async await postRegister

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {postRegister} = require('../controllers/index') 
+const {postRegister} = require('../controllers'); 
+const {errorHandler} = require('../middlewares'); 
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -13,7 +14,7 @@ router.get('/register', (req, res, next) => {
 });
 
 
-router.post('/register', postRegister);
+router.post('/register', errorHandler(postRegister));
 
 
 router.get('/login', (req, res, next) => {
