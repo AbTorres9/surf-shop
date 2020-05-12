984cf564a19fa05892dfafea293a5d27aa623b16
AbTorres9 <you@example.com>
2020-05-12 18:09:14 +0530

    login logout conrollers

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {postRegister} = require('../controllers'); 
+const {postRegister, postLogin, getLogout} = require('../controllers'); 
 const {errorHandler} = require('../middlewares'); 
 
 /* GET home page. */
@@ -22,9 +22,10 @@ router.get('/login', (req, res, next) => {
 });
 
 
-router.post('/login', (req, res, next) => {
-  res.send('POST /login');
-});
+router.post('/login', postLogin);
+
+
+router.get('/logout', getLogout);
 
 
 router.get('/profile', (req, res, next) => {
